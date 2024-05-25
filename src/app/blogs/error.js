22fb4b09ff0528aa5da390032475800fb2ff3091
AYMEN GHERDAINE <gherdaineaymen1995@gmail.com>
2024-05-24 20:41:22 -0400

    adding env variables

--- src/app/blogs/error.js
+++ src/app/blogs/error.js
@@ -3,13 +3,14 @@
 import Image from "next/image";
 import styles from "@/app/blogs/blogs.module.css";
 import RevealAnimation from "@/components/Animation/RevealAnimation";
+import not_found from "../../../public/not-found.png";
 
 const Error = () => {
   return (
     <RevealAnimation delay="0.8">
       <main className={styles.not_found_wrapper}>
         <Image
-          src={`${process.env.AWS_S3_BUCKET}/Not-Found.png`}
+          src={not_found}
           alt="image 404 not found if there is an error that occured"
           width={500}
           height={500}
