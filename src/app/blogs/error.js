ec878b7e424480f126326e65b2967f588c62694e
AYMEN GHERDAINE <gherdaineaymen1995@gmail.com>
2024-05-28 20:28:23 -0400

    adding error page image

--- src/app/blogs/error.js
+++ src/app/blogs/error.js
@@ -3,14 +3,13 @@
 import Image from "next/image";
 import styles from "@/app/blogs/blogs.module.css";
 import RevealAnimation from "@/components/Animation/RevealAnimation";
-import not_found from "../../../public/not-found.png";
 
 const Error = () => {
   return (
     <RevealAnimation delay="0.8">
       <main className={styles.not_found_wrapper}>
         <Image
-          src={not_found}
+          src="../../../public/not-found.png"
           alt="image 404 not found if there is an error that occured"
           width={500}
           height={500}
